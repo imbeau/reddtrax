40afb607c65abe1114fd98a98aec5050f1a711c9
imbeau <Beau@Beaus-MacBook-Pro.local>
2017-09-21 18:54:22 -0400

    added layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'trackers/new'
+  resources :trackers
 
+  # I seem to have to keep these here to get tests to continue to work:
+  get 'trackers/new'
   get 'trackers/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/controllers/trackers_controller_test.rb
+++ test/controllers/trackers_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class TrackersControllerTest < ActionDispatch::IntegrationTest
+
+
+	def setup
+		@base_title = "ReddTrax"
+	end
+
   test "should get new" do
     get trackers_new_url
     assert_response :success
+    assert_select "title", "Create New Tracker | #{@base_title}"
   end
 
   test "should get index" do
     get trackers_index_url
     assert_response :success
+    assert_select "title", "View all Trackers | #{@base_title}"
   end
 
 end
